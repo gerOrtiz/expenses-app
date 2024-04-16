fa51a04cee429631942b33a01ef85f86cab1e9aa
Gerson <ger.ortiz7@gmail.com>
2024-04-15 18:23:56 -0600

    Update dependencies and env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    MONGODB_USER: '',
-    MONGODB_PASS: '',
+    MONGODB_USER: 'gersonortiz',
+    MONGODB_PASS: 'AH8Bi78pwXSF5GJ5',
     NEXTAUTH_SECRET: 'wbs4k4qkg7pqtdet7zza5kw2ehn4goj7xmjzq2hijsvgm2dwqquq',
-  }
+  },
+  swcMinify: true
 };
 
 export default nextConfig;
