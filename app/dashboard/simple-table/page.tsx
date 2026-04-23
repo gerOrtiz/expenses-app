c0486e36501498a2df18e4a0344de411d64db525
Gerson <ger.ortiz7@gmail.com>
2026-04-23 10:52:03 -0600

    Update README file, fix logout bug in simple table

--- app/dashboard/simple-table/page.tsx
+++ app/dashboard/simple-table/page.tsx
@@ -1,16 +1,27 @@
 
 import SimpleTableLayoutComponent from "@/components/dashboard/layout/simpleTableLayout";
+import ExpensesPageSkeleton from "@/components/loadingSkeletons/expensesPageSkeleton";
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
-export default function SimpleExpensesPage() {
-
-	const wrapper = (
+export default function SimpleExpenses() {
+	return (
 		<main className="flex min-h-max flex-col py-2">
 			<div className="relative flex-1 lg:container text-center p-0 mx-auto overflow-x-hidden overflow-auto">
 				<h1 className="sr-only">Expenses Table</h1>
-				<SimpleTableLayoutComponent />
+				<Suspense fallback={<ExpensesPageSkeleton />}>
+					<SimpleExpensesPage />
+				</Suspense>
 			</div>
 		</main>
 	);
+}
 
-	return wrapper;
+async function SimpleExpensesPage() {
+	const session = await getServerSession(authOptions);
+
+	if (!session) redirect('/login');
+	return <SimpleTableLayoutComponent />
 }
